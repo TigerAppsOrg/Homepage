5a6eb9e86c2bb567320dcb5fe160d084154ec8fb
joshuamotoaki <dachshunduniverse@gmail.com>
2024-08-17 17:31:34 -0600

    Begin to create member card

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                "primary": "#3EE0BF",
+                "on-primary": "#383838",
+                "secondary": "#2B3E50",
+                "on-secondary": "#FFFFFF"
+            }
+        }
     },
     plugins: []
 };
