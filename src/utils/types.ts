a5568a8dd425a370499c8c274a044205f2e708c4
Joshua Lau <dachshunduniverse@gmail.com>
2025-06-14 14:57:23 -0700

    feat: Generalize contributor descriptions

---
+++ src/utils/types.ts
@@ -0,0 +1,18 @@
+export type Contributor = { name: string; url?: string };
+
+export type ContributorSection = {
+    startMsg: string;
+    contributors: Contributor[];
+};
+
+export interface AppProps {
+    title: string;
+    description: string;
+    note: string;
+    webLink: string;
+    repoLink: string;
+    screenshot: ImageMetadata;
+    alt: string;
+    contributors?: Contributor[];
+    additionalContributors?: ContributorSection[];
+}
